17adb20f00a0462c80d48d0058f0c23d61585fc0
QCXYLTJ <104590442+QCXYLTJ@users.noreply.github.com>
2025-06-02 23:46:13 +0800

    删除全装备

--- character.js
+++ character.js
@@ -193,10 +193,6 @@ const character = {
         sex: 'female',
         skills: ['QQQ_xipai', 'QQQ_zhang'],
     },
-    QQQ_全装备1: {
-        sex: 'male',
-        skills: ['全装备', 'gzjili', 'olqiangxi', 'qiaoli', 'olsbdulie'],
-    },
     QQQ_全装备: {
         sex: 'male',
         skills: ['全装备', 'xiaoji', 'olqizhou', 'xuanfeng', 'ganlu'],
@@ -511,8 +507,7 @@ const translate2 = {
     QQQ_神裁: '神裁',
     QQQ_哦哦: '哦哦',
     QQQ_夫子: '夫子',
-    QQQ_全装备1: '全装备1',
-    QQQ_全装备: '全装备2',
+    QQQ_全装备: '全装备',
     QQQ_西素惊鸿: '西素惊鸿',
     QQQ_复活: '复活',
     QQQ_穆穆: '穆穆',
@@ -542,7 +537,7 @@ const translate2 = {
     QQQ_许贡: '许贡',
 };
 const sort = {
-    技能堆叠: ['QQQ_冯方女', 'QQQ_神吕布', 'QQQ_距离掌控者', 'QQQ_装备', 'QQQ_反面', 'QQQ_体力上限', 'QQQ_体力游戏', 'QQQ_天妒', 'QQQ_黄盖', 'QQQ_黄盖1', 'QQQ_拼点', 'QQQ_南蛮', 'QQQ_判定', 'QQQ_吴懿关张', 'QQQ_孟获', 'QQQ_食尸鬼', 'QQQ_势力', 'QQQ_董白', 'QQQ_诸葛亮', 'QQQ_关张刘焉', 'QQQ_驭衡', 'QQQ_全装备1', 'QQQ_全装备', 'QQQ_不疑曹轶', 'QQQ_天师马师', 'QQQ_伤逝约俭', 'QQQ_贾诩王凌', 'QQQ_灵毓陈宫', 'QQQ_满宠芳兰', 'QQQ_太后朱然', 'QQQ_花鬘张飞', 'QQQ_关羽管宁', 'QQQ_笮融陈宫', 'QQQ_卢弈辛毗', 'QQQ_沮授陈宫', 'QQQ_摸与杀', 'QQQ_潜水的火', 'QQQ_qianbian'],
+    技能堆叠: ['QQQ_冯方女', 'QQQ_神吕布', 'QQQ_距离掌控者', 'QQQ_装备', 'QQQ_反面', 'QQQ_体力上限', 'QQQ_体力游戏', 'QQQ_天妒', 'QQQ_黄盖', 'QQQ_黄盖1', 'QQQ_拼点', 'QQQ_南蛮', 'QQQ_判定', 'QQQ_吴懿关张', 'QQQ_孟获', 'QQQ_食尸鬼', 'QQQ_势力', 'QQQ_董白', 'QQQ_诸葛亮', 'QQQ_关张刘焉', 'QQQ_驭衡', 'QQQ_全装备', 'QQQ_不疑曹轶', 'QQQ_天师马师', 'QQQ_伤逝约俭', 'QQQ_贾诩王凌', 'QQQ_灵毓陈宫', 'QQQ_满宠芳兰', 'QQQ_太后朱然', 'QQQ_花鬘张飞', 'QQQ_关羽管宁', 'QQQ_笮融陈宫', 'QQQ_卢弈辛毗', 'QQQ_沮授陈宫', 'QQQ_摸与杀', 'QQQ_潜水的火', 'QQQ_qianbian'],
     惊艳一枪: [],
     温柔一刀: [],
     伤心一箭: [],
